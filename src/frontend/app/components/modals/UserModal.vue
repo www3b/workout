c033bbe7790d11c4abf7921822369ad90c6ffe84
Pavel <p.sharafiev@gmail.com>
2026-01-14 17:25:53 +0300

    fix: User creation modal fix

--- src/frontend/app/components/modals/UserModal.vue
+++ src/frontend/app/components/modals/UserModal.vue
@@ -1,24 +1,52 @@
 <script setup lang="ts">
-type Props = {
-  open: boolean;
-};
+import * as z from "zod";
+type FormSubmitEvent<T> = SubmitEvent & { data: T };
 
-defineProps<Props>();
+const schema = z.object({
+  name: z.string("Name is required").min(2, "Must be at least 2 characters"),
+  email: z.email("Invalid email"),
+  password: z
+    .string("Password is required")
+    .min(8, "Must be at least 8 characters"),
+});
 
-const name = ref("");
-const email = ref("");
+type Schema = z.output<typeof schema>;
+
+const state = reactive<Partial<Schema>>({
+  name: undefined,
+  email: undefined,
+  password: undefined,
+});
+
+const toast = useToast();
+
+async function onSubmit(event: FormSubmitEvent<Schema>) {
+  toast.add({
+    title: "Success",
+    description: "The form has been submitted.",
+    color: "success",
+  });
+  console.log(event.data);
+}
 </script>
 
 <template>
-  <SimpleModal :is-open="open" title="User Details">
+  <SimpleModal :is-open="true" title="User Details">
     <slot />
     <template #body>
-      <UFormField label="Name" required>
-        <UInput label="Name" v-model="name" class="mb-4" />
-      </UFormField>
-      <UFormField label="Email" required>
-        <UInput label="Email" v-model="email" class="mb-4" />
-      </UFormField>
+      <UForm :schema="schema" :state="state" @submit.prevent="onSubmit">
+        <UFormField label="Name" name="name" required>
+          <UInput v-model="state.name" class="mb-4" />
+        </UFormField>
+        <UFormField label="Email" name="email" required>
+          <UInput v-model="state.email" class="mb-4" />
+        </UFormField>
+        <UFormField label="Password" name="password" required>
+          <UInput v-model="state.password" class="mb-4" />
+        </UFormField>
+
+        <UButton type="submit"> Submit </UButton>
+      </UForm>
     </template>
   </SimpleModal>
 </template>
